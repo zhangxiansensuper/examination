01d6747151d18da72603fdbb7d889f317d8cfa34
zhang13080539507@163.com <zhang13080539507@163.com>
2019-06-24 15:58:26 +0800

    使用递归逆序一个栈

---
+++ src/chapter1/Reverse.java
@@ -0,0 +1,39 @@
+package chapter1;
+
+import java.util.Stack;
+
+public class Reverse {
+
+	public static int getAndRemoveLastElement(Stack<Integer> stack){
+		int result = stack.pop();
+		if(stack.empty()){
+			return result;
+		}else {
+			int last = getAndRemoveLastElement(stack);
+			stack.push(result);
+			return last;
+		}
+	}
+	
+	public static void reverse(Stack<Integer> stack){
+		if (stack.isEmpty()) {
+			return;
+		}
+		int i = getAndRemoveLastElement(stack);
+		reverse(stack);
+		stack.push(i);
+	}
+	
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(1);
+		stack.push(2);
+		stack.push(3);
+		Reverse.reverse(stack);
+//		System.out.println(stack.size()+"--------");
+		while(!stack.isEmpty()){
+			System.out.println(stack.pop());
+		}
+		
+	}
+}
